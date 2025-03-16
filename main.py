311de6d026b6cc8571c2044fc8cb3e724bde44d1
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2025-03-16 11:02:58 +0200

    fixed the example file

--- main.py
+++ main.py
@@ -23,14 +23,5 @@ async def main():
         print("After Delete:", deleted_data)
 
 
-async def test():
-    repository = DinORM("http://localhost", port=8000, debug=False)
-
-    resp = await repository.update("01c08731-3860-4ca9-b1d8-bf85f13c131c", { "changed": "true" })
-    print("Response: ", resp)
-
-    data = await repository.get("01c08731-3860-4ca9-b1d8-bf85f13c131c")
-    print("Data: ", data)
-
 if __name__ == "__main__":
-    asyncio.run(test())
+    asyncio.run(main())
